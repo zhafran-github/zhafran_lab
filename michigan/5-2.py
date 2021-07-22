7059168fd70aba51f9bae9c5a2c38e5242a98707
Zhafran <87810356+zhafran-github@users.noreply.github.com>
2021-07-23 00:10:53 +0700

    Update 5-2.py
    inserting while and error handling

--- michigan/5-2.py
+++ michigan/5-2.py
@@ -5,5 +5,9 @@ smallest = None
 #read number from a user
 x= input('Enter Number:')
 
+#keep asking input until user submit the word 'done'
+while x != 'done' :
+    #using try and except for error handling
+    try :
 
-    
+    except:
