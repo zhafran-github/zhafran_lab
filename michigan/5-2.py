117b8d8e5397da8e358de7de310128f5367bef32
Zhafran <87810356+zhafran-github@users.noreply.github.com>
2021-07-23 00:26:02 +0700

    Update 5-2.py
    adding problem description and desired output

--- michigan/5-2.py
+++ michigan/5-2.py
@@ -1,3 +1,14 @@
+# Write a program that repeatedly prompts a user for integer numbers until the user enters 'done'. 
+# Once 'done' is entered, print out the largest and smallest of the numbers. 
+# If the user enters anything other than a valid number catch it with a try/except 
+# and put out an appropriate message and ignore the number. 
+# Enter 7, 2, bob, 10, and 4 and match the output below. 
+
+# Desired Ouput
+# Invalid Input  
+# Maximum is 10 
+# Minimum is 2 
+
 #create flag
 largest = -1
 smallest = None
@@ -26,7 +37,7 @@ while x != 'done' :
     except:
         print('Invalid input')
         x = input('Enter Number:')
-        
+
 #print max and min
 print('Maximum is', largest)
 print('Minimum is', smallest)
