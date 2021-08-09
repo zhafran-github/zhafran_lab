573286cbc3241b8d7a5465fddf4c37485f29e5a6
Zhafran <87810356+zhafran-github@users.noreply.github.com>
2021-08-10 01:00:02 +0700

    v0
    create an object

---
+++ michigan/Chapter 2 - Python Data Structure/7.2.py
@@ -0,0 +1,5 @@
+# calculate the average of float number after the words "X-DSPAM-Confidence : "
+file_name = input('Enter the file name : ')
+#create an object
+f = open(file_name)
+#variable the amout
