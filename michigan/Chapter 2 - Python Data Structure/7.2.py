24a69ade55f620aaf16498d4a61b3da7e81c0a99
Zhafran <87810356+zhafran-github@users.noreply.github.com>
2021-08-10 01:03:41 +0700

    Update 7.2.py
    add total_float

--- michigan/Chapter 2 - Python Data Structure/7.2.py
+++ michigan/Chapter 2 - Python Data Structure/7.2.py
@@ -3,3 +3,6 @@ file_name = input('Enter the file name : ')
 #create an object
 f = open(file_name)
 #variable to put the amount of data containing the word we are looking for
+n = 0
+#variable to sum all of the float we get from the words
+total_float = 0
