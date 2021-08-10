207b28f95f00d50e9e15e5c248a9bf580c79b0ed
Zhafran <87810356+zhafran-github@users.noreply.github.com>
2021-08-10 21:32:56 +0700

    Update 7-2.py
    final

--- michigan/Chapter 2 - Python Data Structure/7-2.py
+++ michigan/Chapter 2 - Python Data Structure/7-2.py
@@ -2,3 +2,22 @@
 file_name = input('Enter the file name : ')
 #create an object
 f = open(file_name)
+#count the amount of lines
+n = 0 
+#sum all float after XDSPAM
+total_of_float = 0 
+#convert n to flot because it will be used as a denominator for average calculation
+nf = float(n)
+for line in f :
+    if not line.startswith('X-DSPAM-Confidence:') : continue
+    n = n + 1
+    #take the number after comma
+    xDSPAM = line[line.find('0') : ]
+    #convert xDSPAM to float
+    xDSPAM_float = float(xDSPAM)
+    #sum all the float we get from scanning the file
+    total_of_float = total_of_float + xDSPAM_float
+xDSPAM_float_average = total_of_float/n
+print('Average spam confidence:', xDSPAM_float_average)
+
+
