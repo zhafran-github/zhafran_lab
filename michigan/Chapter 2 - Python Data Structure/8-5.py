1fd9057a5e37528dd2dec61ccb9d3d9cb8493c94
Zhafran <87810356+zhafran-github@users.noreply.github.com>
2021-08-11 16:23:58 +0700

    Update 8-5.py
    file initiation

--- michigan/Chapter 2 - Python Data Structure/8-5.py
+++ michigan/Chapter 2 - Python Data Structure/8-5.py
@@ -2,3 +2,10 @@
 # You will parse the From line using split() and print out the second word in the line (i.e. the entire address of the person who sent the message). 
 # Then print out a count at the end.  
 # Hint: make sure not to include the lines that start with 'From:'. Also look at the last line of the sample output to see how to print the count. 
+
+fname = input("Enter file name: ")
+if len(fname) < 1:
+    fname = "mbox-short.txt"
+
+fh = open(fname)
+count = 0
